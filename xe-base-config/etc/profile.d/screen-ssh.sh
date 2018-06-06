04db7caf22cc2c4a07e32e1ec7f01144e3988bd0
Jakobus Schürz <jakob@xundeenergie.at>
2018-06-06 22:11:51 +0200

    xe20180606_221151

--- xe-base-config/etc/profile.d/screen-ssh.sh
+++ xe-base-config/etc/profile.d/screen-ssh.sh
@@ -1,5 +1,5 @@
 [ -z "$XDG_RUNTIME_DIR" ] && export XDG_RUNTIME_DIR=/run/user/$(id -u $USER)
-if test $SSH_TTY && test $TERM != screen ; then
+if test $SSH_TTY && test $TERM != screen && test $(systemctl status screen@${USER} 1>/dev/null 2>&1; echo $? ) -eq 0; then
         echo Starting or resuming screen session
         echo Press CTRL+C to cancel screen startup
 	echo "User: $USER - $UID"
